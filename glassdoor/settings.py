387db331638080f6fd79c13dc433941479cc9c77
ZhouYanhui <364267040@qq.com>
2023-05-13 16:46:20 +0800

    添加.DS_Store

---
+++ glassdoor/settings.py
@@ -0,0 +1,22 @@
+# -*- coding = utf-8 -*-
+# @Time: 2022/9/19 18:24
+# 这里是配置文件,启动程序之前,确定这里已经设置正确
+
+# 谷歌浏览器驱动
+WEBDRIVER_PATH = '/Users/zhouyanhui/Library/CloudStorage/OneDrive-个人/chromedriver'
+
+# 登录glassdoor的账号密码
+USERNAME = '759388790@qq.com'
+PASSWORD = 'erhui7169'
+
+# 起始的url,
+START_URL = 'https://www.glassdoor.com/Reviews/Apple-Reviews-E1138_P%d.htm'
+
+# 公司名称
+COMPANY_NAME = 'Apple'
+
+# 这里填reviews总数
+REVIEWS_PAGE = 18468
+
+# 设置一次运行程序爬取多少页,0为不限制,抓取所有
+GET_PAGE = 10
